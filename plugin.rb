07b4bdf37b5bbb379424c1bebdc747a966fb9803
Jesse Griffin <jag3773@gmail.com>
2018-05-29 14:32:51 -0400

    Bumped version number

--- plugin.rb
+++ plugin.rb
@@ -1,5 +1,5 @@
 # name: discourse-mermaid
 # about: Adds the [Mermaid JS](https://mermaidjs.github.io/) library
-# version: 0.0.1
+# version: 0.0.2
 # authors: unfoldingWord
 # url: https://github.com/unfoldingWord-dev/discourse-mermaid
